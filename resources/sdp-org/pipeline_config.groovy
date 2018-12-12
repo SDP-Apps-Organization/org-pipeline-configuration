3205c6d7f2a7b129d62ae9d5014c1cc5a2740032
Alec Keller <keller_alec@bah.com>
2018-12-12 12:10:40 -0500

    added config

---
+++ resources/sdp-org/pipeline_config.groovy
@@ -0,0 +1,8 @@
+//Organizational Config File
+application_image_repository = "aleckeller13/sdp-applications"
+application_image_repository_credential = "docker-creds"
+
+libraries{
+  github_enterprise
+  docker
+}
---
+++ vars/do_nothing.groovy
@@ -0,0 +1,1 @@
+//Needed for Jenkins to load as a shared library. This file is supposed to be empty.
