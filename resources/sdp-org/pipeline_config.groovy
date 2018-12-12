93a528b388cceb4a7aafed91d81b65269f67ade1
Alec Keller <keller_alec@bah.com>
2018-12-12 12:11:37 -0500

    changed dockerhub creds

--- resources/sdp-org/pipeline_config.groovy
+++ resources/sdp-org/pipeline_config.groovy
@@ -1,6 +1,6 @@
 //Organizational Config File
 application_image_repository = "aleckeller13/sdp-applications"
-application_image_repository_credential = "docker-creds"
+application_image_repository_credential = "dockerhub-creds"
 
 libraries{
   github_enterprise
