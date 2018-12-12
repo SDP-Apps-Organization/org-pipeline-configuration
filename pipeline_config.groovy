008a08f614276b93356579624b64f65ffadab3c0
Alec Keller <keller_alec@bah.com>
2018-12-12 12:42:35 -0500

    added jenkinsfile and pipeline config to root

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,4 +1,3 @@
-//Organizational Config File
 application_image_repository = "aleckeller13/sdp-applications"
 application_image_repository_credential = "dockerhub-creds"
 
--- vars/do_nothing.groovy
+++
@@ -1,1 +0,0 @@
-//Needed for Jenkins to load as a shared library. This file is supposed to be empty.
