5c3de7902ba0350f49d85776ed5352730252e037
Alec Keller <keller_alec@bah.com>
2018-12-17 15:09:33 -0500

    added app envs and logic

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,4 +1,26 @@
-on_commit{
+on_commit to: develop{
   build()
-  api_testing()
+  api_testing(dev)
+  //deploy to dev
+}
+on_commit to: release{
+  build()
+  api_testing(staging)
+  //deploy to staging
+}
+on_commit to: hotfix{
+  build()
+  api_testing(hotfix)
+  //deploy to hotfix
+}
+
+on_pull_request to: develop{
+  build()
+  api_testing(dev)
+  //deploy
+}
+
+on_pull_request to: master{
+  build()
+  api_testing(prod)
 }
--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -8,3 +8,41 @@ libraries{
     merge = true
   }
 }
+
+keywords{
+  master  =  /^[Mm]aster$/
+  develop =  /^[Dd]evelop(ment|er|)$/
+  hotfix  =  /^[Hh]ot[Ff]ix-/
+  release =  /^[Rr]elease-(\d+.)*\d$/
+}
+
+application_environments{
+     dev{
+         short_name = "comp"
+         long_name = "Component"
+         appName = "dev"
+         ucp_bundle_directory = "/var/lib/jenkins/bundle_nonprod"
+         askApproval = true
+     }
+     test{
+         short_name = "minc"
+         long_name = "Minimum-Capacity"
+         appName = "test"
+         ucp_bundle_directory = "/var/lib/jenkins/bundle_nonprod"
+         askApproval = true
+     }
+     staging{
+         short_name = "prodlike"
+         long_name = "Production-Like"
+         appName = "staging"
+         ucp_bundle_directory = "/var/lib/jenkins/bundle_nonprod"
+         askApproval = true
+     }
+     prod{
+       short_name = "prod"
+       long_name = "Production"
+       appName = "prod"
+       ucp_bundle_directory = "/var/lib/jenkins/prod"
+       askApproval = true
+     }
+}
